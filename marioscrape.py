03cb9bb48085f3e8ed76d565db6d391f7ccc3780
tylerkerr <kerr.tyler@gmail.com>
2023-02-23 22:00:46 -0500

    initial commit

---
+++ marioscrape.py
@@ -0,0 +1,302 @@
+import sqlite3
+import os
+import json
+import pandas
+from requests import get
+from datetime import datetime, timezone
+from dateutil import parser
+from dateutil.relativedelta import relativedelta
+from time import sleep
+from re import search, sub
+
+
+'''
+sample killmail object:
+{
+  "id": "1eda1cae-b0fd-6ace-ad38-7fc908788205",
+  "report_id": 8108929,
+  "killer_corp": "SHIE",
+  "killer_name": "Agnes Milfstein",
+  "isk": 20261432,
+  "date_killed": "2023-02-01 00:34:02",
+  "image_url": "https://echoes.mobi/killmails/8108929.png",
+  "victim_ship_type": "Coercer Interdictor",
+  "victim_ship_category": "Destroyer",
+  "external_provider": null,
+  "victim_name": "CyberJinn",
+  "victim_corp": "SELF",
+  "system": "VFK-IV",
+  "constellation": "VW7-YN",
+  "region": "Deklein",
+  "victim_total_damage_received": 3809,
+  "total_participants": 6,
+  "killer_ship_type": "Tempest Striker",
+  "killer_ship_category": "Battleship"
+}
+'''
+
+# constants
+
+db_name = 'km.db'
+baseurl = 'https://echoes.mobi/killboard/export'
+headers = {
+    'User-Agent': 'honk-mariobot-parse/0.1 (bearand#4133@discord)'
+}
+
+launch_date = "2020-08-13"
+
+
+# database setup
+
+def db_init(db_name):
+    global conn
+    conn = sqlite3.connect(db_name)
+    conn.row_factory = sqlite3.Row
+    global c
+    c = conn.cursor()
+
+
+def db_open(db_name):
+    if os.path.exists(db_name):
+        db_init(db_name)
+        print("[+] db connection established")
+    else:
+        db_init(db_name)
+        print("[-] initializing database")
+        create_tables(db_name)
+        print('[+] db initialized')
+
+
+def create_tables(db_name):
+    c.execute('DROP TABLE IF EXISTS "Killmails"')
+    c.execute('DROP TABLE IF EXISTS "Status"')
+    c.execute('DROP TABLE IF EXISTS "Months"')
+    c.executescript('''
+    CREATE TABLE "Killmails" (
+	"id"	                        TEXT NOT NULL PRIMARY KEY UNIQUE,
+	"report_id"	                    TEXT NOT NULL,
+    "killer_corp"	                TEXT,
+    "killer_name"                   TEXT NOT NULL,
+    "isk"                           INTEGER NOT NULL,
+    "date_killed"                   TEXT NOT NULL,
+    "image_url"                     TEXT NOT NULL,
+    "victim_ship_type"              TEXT,
+    "victim_ship_category"          TEXT,
+    "external_provider"             TEXT,
+    "victim_name"                   TEXT,
+    "victim_corp"                   TEXT,
+    "system"                        TEXT,
+    "constellation"                 TEXT,
+    "region"                        TEXT,
+    "victim_total_damage_received"  INTEGER,
+    "total_participants"            INTEGER,
+    "killer_ship_type"              TEXT,
+    "killer_ship_category"          TEXT,
+    "timestamp"                     INTEGER
+    );
+    CREATE INDEX IF NOT EXISTS "IX_ISK" ON "Killmails" (
+	"isk"
+    );
+    CREATE INDEX IF NOT EXISTS "IX_TotalDamage" ON "Killmails" (
+	"victim_total_damage_received"
+    );
+    CREATE INDEX IF NOT EXISTS "IX_TotalParticipants" ON "Killmails" (
+	"total_participants"
+    );
+    CREATE INDEX IF NOT EXISTS "IX_Timestamp" ON "Killmails" (
+    "timestamp"
+    );
+    CREATE INDEX IF NOT EXISTS "IX_RecordID" ON "Killmails" (
+    "record_id"
+    );
+    ''')
+
+    c.execute('''
+    CREATE TABLE "Status" (
+    "last_refresh_time"     TEXT NOT NULL
+    );
+    ''')
+
+    c.execute('''
+    CREATE TABLE "Months" (
+    "month"             TEXT NOT NULL PRIMARY KEY UNIQUE,
+    "last_refreshed"    TEXT NOT NULL
+    );
+    ''')
+    conn.commit()
+
+
+
+# date stuff
+
+def is_this_month(month_iso):
+    if month_iso == get_this_month():
+        return True
+    return False
+
+
+def is_month_current(month_iso):
+    print(f"[-] checking if month {month_iso} is current")
+    result = check_month_status(month_iso)
+    if not result:
+        return False
+    status = parser.isoparse(result)
+    now = datetime.now(timezone.utc)
+    if not status:
+        print(f"[-] no data for month {month_iso}")
+        return False
+    difference = now - status
+    data_age_hours = difference.seconds / 60 / 24
+    if is_this_month(month_iso):
+        limit = 2
+    else:
+        limit = 168
+    if data_age_hours <= limit:
+        print(f"[+] month {month_iso} is {data_age_hours} hours old, current")
+        return True
+    print(f"[+] month {month_iso} is {data_age_hours} hours old, too old")
+    return False
+
+
+def get_today_american():
+    # american format
+    return datetime.now(timezone.utc).strftime("%m-%d-%Y")
+
+
+def get_today_iso():
+    return datetime.now(timezone.utc).strftime("%Y-%m-%d")
+
+
+def get_this_month():
+    return datetime.now(timezone.utc).strftime("%Y-%m")
+
+
+def get_all_months():
+    return pandas.date_range(parser.isoparse(launch_date).replace(day=1), get_today_iso(), freq='MS').strftime("%Y-%m").tolist()
+
+
+# queries
+
+
+def query(query_text, args=(), one=False):
+    cursor = conn.execute(query_text, args)
+    results = cursor.fetchall()
+    cursor.close()
+    return (results[0] if results else None) if one else results
+
+
+def get_report_id(report_id):
+    c.execute(f'SELECT * FROM Killmails WHERE report_id={report_id}')
+    return dict(c.fetchone())
+
+
+def check_month_status(month_iso):
+    c.execute(f'SELECT * FROM Months WHERE month="{month_iso}"')
+    fetch = c.fetchone()
+    if fetch:
+        result = fetch[1]
+        print(f"[-] the result of checking month {month_iso} was {result}")
+        return result
+    else:
+        return None
+
+
+def update_month_status(month):
+    now = datetime.now(timezone.utc).isoformat()
+    c.execute(
+        'REPLACE INTO Months (month, last_refreshed) VALUES (?, ?)', (month, now))
+    print(f"[+] updated month {month} with last_refreshed of {now}")
+    conn.commit()
+
+
+def write_km_dict(km_dict):
+    c.executemany('''
+                INSERT OR IGNORE INTO "Killmails"
+                (id, report_id, killer_corp, killer_name, isk, date_killed,
+                 image_url, victim_ship_type, victim_ship_category, external_provider,
+                 victim_name, victim_corp, system, constellation, region,
+                 victim_total_damage_received, total_participants, killer_ship_type, 
+                 killer_ship_category, timestamp)
+                VALUES
+                (:id, :report_id, :killer_corp, :killer_name, :isk, :date_killed,
+                 :image_url, :victim_ship_type, :victim_ship_category, :external_provider,
+                 :victim_name, :victim_corp, :system, :constellation, :region,
+                 :victim_total_damage_received, :total_participants, :killer_ship_type, 
+                 :killer_ship_category, :timestamp)
+              ''', km_dict)
+    conn.commit()
+
+# scraping
+
+
+def download_export(start, end):
+    url = f"{baseurl}/{start}/{end}"
+    response = get(url, headers=headers)
+    if response.status_code == 200:
+        return response.text
+    else:
+        return False
+
+
+def download_month(month):
+    start_datetime = parser.isoparse(month).replace(day=1)
+    start_date = start_datetime.strftime("%m-%d-%Y")
+    end_datetime = (start_datetime + relativedelta(months=1))
+    end_date = end_datetime.strftime("%m-%d-%Y")
+    km_json = json.loads(download_export(start_date, end_date))
+    print(f"[+] got month {month} with {len(km_json)} killmails")
+    stamped_kms = []
+    for km in km_json:
+        if km['external_provider'] == 'auxilus' and km['victim_corp'] == None and km['victim_name']:
+            victim_corp = search(r'\[[A-Z0-9]{1,4}\] ', km['victim_name'])
+            if victim_corp:
+                km['victim_corp'] = victim_corp[0][1:len(victim_corp[0])-2]
+                km['victim_name'] = km['victim_name'][len(victim_corp[0]):]
+
+        if km['killer_corp'] and (len(str(km['killer_corp'])) > 4 or ']' in str(km['killer_corp'])):
+            if ']' in str(km['killer_corp']):
+                problem = search(r'\].*', str(km['killer_corp']))[0]
+                km['killer_corp'] = km['killer_corp'][:len(km['killer_corp']) - len(problem)]
+            if len(km['killer_corp']) > 4:
+                km['killer_corp'] = km['killer_corp'][:4]
+
+        if km['victim_corp'] and (len(str(km['victim_corp'])) > 4 or ']' in str(km['victim_corp'])):
+            fixed = sub(r'[^a-zA-Z0-9]', '', str(km['victim_corp']))
+            if len(fixed) > 4:
+                if fixed[0] == 'I':
+                    fixed = fixed[1:]
+                else:
+                    fixed = fixed[:4]
+            km['victim_corp'] = fixed
+
+        timestamp = int(parser.isoparse(km['date_killed']).timestamp())
+        km['timestamp'] = timestamp
+        stamped_kms.append(km)
+    return stamped_kms
+
+
+
+if __name__ == "__main__":
+    db_open(db_name)
+
+    km_count = query('select count(distinct id) from killmails;', one=True)
+    print(f"[-] db has {km_count[0]} killmails")
+
+    all_months = get_all_months()
+    print(all_months)
+
+    # month_json = download_month('2022-09')
+    # write_km_dict(month_json)
+
+    for month in get_all_months():
+        if not is_month_current(month):
+            print(f"[-] downloading month {month}")
+            month_json = download_month(month)
+            write_km_dict(month_json)
+            update_month_status(month)
+            sleep(5) # be nice to mario
+        else:
+            print(f"[-] skipping month {month}, already have it")
+
+    c.close()
+    conn.close()
---
+++ mvweb/app.py
@@ -0,0 +1,216 @@
+from os import environ, path
+from flask import Flask, Blueprint, render_template, request, url_for, redirect, g
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.sql import func, text
+# from km_db import close_connection
+from werkzeug.exceptions import HTTPException, InternalServerError, BadRequest
+from werkzeug.middleware.proxy_fix import ProxyFix
+# from views import routes
+from glob import glob
+from hashlib import md5
+from re import sub
+import logging
+import base64
+import time
+
+
+app = Flask(__name__)
+
+app.wsgi_app = ProxyFix(app.wsgi_app)
+app.jinja_env.trim_blocks = True
+app.jinja_env.lstrip_blocks = True
+# register(app.jinja_env)
+
+db_name = '../km.db'
+basedir = path.abspath(path.dirname(__file__))
+db_path = 'sqlite:///' + path.join(basedir, db_name)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = db_path
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+class Killmails(db.Model):
+    id = db.Column(db.String, primary_key=True, nullable=False, unique=True)
+    report_id = db.Column(db.String, nullable=False)
+    killer_corp = db.Column(db.String)
+    killer_name = db.Column(db.String, nullable=False)
+    isk = db.Column(db.Integer, nullable=False)
+    date_killed = db.Column(db.String, nullable=False)
+    image_url = db.Column(db.String, nullable=False)
+    victim_ship_type = db.Column(db.String)
+    victim_ship_category = db.Column(db.String)
+    external_provider = db.Column(db.String)
+    victim_name = db.Column(db.String)
+    victim_corp = db.Column(db.String)
+    system = db.Column(db.String)
+    constellation = db.Column(db.String)
+    region = db.Column(db.String)
+    victim_total_damage_received = db.Column(db.Integer)
+    total_participants = db.Column(db.Integer)
+    killer_ship_type = db.Column(db.String)
+    killer_ship_category =db.Column(db.String)
+    timestamp = db.Column(db.Integer)
+
+    def __repr__(self):
+        return f'<Killmail {self.report_id}>'
+
+
+# @app.errorhandler(BadRequest)
+# def handle_bad_request(e):
+#     return 'bad request!', 400
+
+@app.errorhandler(405)
+def method_not_allowed(error):
+    return redirect("/", code=302)
+
+
+if environ.get('MARIOVIEW_ENVIRONMENT') == "production":
+    app.secret_key = environ.get('MARIOVIEW_FLASK_KEY')
+    app.config.update(SESSION_COOKIE_SECURE=True,
+                      SESSION_COOKIE_HTTPONLY=True,
+                      SESSION_COOKIE_SAMESITE='Strict')
+else:
+    app.secret_key = 'debug'
+
+mutable_static = glob(path.join(app.root_path, 'static/style', '*.css')) + \
+    glob(path.join(app.root_path, 'static/script', '*.js'))
+    
+hash_obj = md5(open(mutable_static[0], 'rb').read())
+
+for static_file in mutable_static[1:]:
+    hash_obj.update(open(static_file, 'rb').read())
+app.config['STATIC_HASH'] = base64.urlsafe_b64encode(
+    hash_obj.digest()).decode('ascii')[:-2]
+
+app.config.from_pyfile(path.join(app.root_path, 'app.cfg'))
+
+routes = Blueprint('views', __name__)
+
+@routes.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.before_request
+def before_request():
+    g.request_start_time = time.time()
+    g.request_time = lambda: "%.5fs" % (time.time() - g.request_start_time)
+
+
+@app.route('/positivity')
+def positivity():
+    corps_poz = db.session.execute(text('''
+                                        SELECT killer_corp, sum(isk)
+                                        FROM Killmails
+                                        GROUP BY killer_corp
+                                        ORDER BY sum(isk)
+    '''))
+
+    corps_neg = db.session.execute(text('''
+                                        SELECT victim_corp, sum(isk)
+                                        FROM Killmails
+                                        GROUP BY victim_corp
+                                        ORDER BY sum(isk)
+    '''))
+
+    score = {}
+
+    for corp in corps_poz:
+        score[corp[0]] = corp[1]
+    for corp in corps_neg:
+        if not corp[0] in score:
+            score[corp[0]] = corp[1] * -1
+        else:
+            score[corp[0]] = score[corp[0]] - corp[1]
+
+    sorted_score = dict(sorted(score.items(), key=lambda item: item[1], reverse=True))
+
+    return render_template('positivity.html', score=sorted_score)
+
+
+
+@app.route('/search', methods=['GET'])
+def search():
+    valid_columns = [c[1] for c in db.session.execute(text('pragma table_info(Killmails)'))]
+    for arg in request.args:
+        if arg not in valid_columns:
+            return "Very bad request", 400
+    order = 'isk'
+    direction = 'desc'
+    report_id = request.args['report_id'] if 'report_id' in request.args and request.args['report_id'] else None
+    killer_corp = '%' + sub('\*', '%', request.args['killer_corp']) if 'killer_corp' in request.args else None
+    killer_name = '%' + sub('\*', '%', request.args['killer_name']) if 'killer_name' in request.args and request.args['killer_name'] else None
+    minimum_isk = request.args['isk'] if 'isk' in request.args and request.args['isk'] else None
+    victim_ship_type = '%' + sub('\*', '%', request.args['victim_ship_type']) if 'victim_ship_type' in request.args else None
+    victim_ship_category = '%' + sub('\*', '%', request.args['victim_ship_category']) if 'victim_ship_category' in request.args else None
+    victim_name = '%' + sub('\*', '%', request.args['victim_name']) if 'victim_name' in request.args else None
+    victim_corp = '%' + sub('\*', '%', request.args['victim_corp']) if 'victim_corp' in request.args else None
+    system = '%' + sub('\*', '%', request.args['system']) if 'system' in request.args else None
+    constellation = '%' + sub('\*', '%', request.args['constellation']) if 'constellation' in request.args else None
+    region = '%' + sub('\*', '%', request.args['region']) if 'region' in request.args else None
+    victim_total_damage_received = request.args['victim_total_damage_received'] if 'victim_total_damage_received' in request.args else None
+    max_total_participants = request.args['total_participants'] if 'total_participants' in request.args else None
+    killer_ship_type = '%' + sub('\*', '%', request.args['killer_ship_type']) if 'killer_ship_type' in request.args else None
+    killer_ship_category = '%' + sub('\*', '%', request.args['killer_ship_category']) if 'killer_ship_category' in request.args else None
+    timestamp = request.args['timestamp'] if 'timestamp' in request.args else None
+    
+    params = {
+              'report_id': report_id,
+              'killer_corp': killer_corp,
+              'killer_name': killer_name,
+              'minimum_isk': minimum_isk,
+              'victim_ship_type': victim_ship_type,
+              'victim_ship_category': victim_ship_category,
+              'victim_name': victim_name,
+              'victim_corp': victim_corp,
+              'system': system,
+              'constellation': constellation,
+              'region': region,
+              'victim_total_damage_received': victim_total_damage_received,
+              'max_total_participants': max_total_participants,
+              'killer_ship_type': killer_ship_type,
+              'killer_ship_category': killer_ship_category,
+              'timestamp': timestamp
+              }
+
+    kms = db.session.execute(text('''
+                                    SELECT * FROM Killmails
+                                    WHERE
+                                    (:report_id IS NULL OR report_id = :report_id) AND
+                                    (:killer_corp IS NULL OR killer_corp LIKE :killer_corp) AND
+                                    (:killer_name IS NULL OR killer_name LIKE :killer_name) AND
+                                    (:minimum_isk IS NULL OR isk >= :minimum_isk) AND
+                                    (:victim_ship_type IS NULL OR victim_ship_type LIKE :victim_ship_type) AND
+                                    (:victim_ship_category IS NULL OR victim_ship_category LIKE :victim_ship_category) AND
+                                    (:victim_name IS NULL OR victim_name LIKE :victim_name) AND
+                                    (:victim_corp IS NULL OR victim_corp LIKE :victim_corp) AND
+                                    (:system IS NULL OR system LIKE :system) AND
+                                    (:constellation IS NULL OR constellation LIKE :constellation) AND
+                                    (:region IS NULL OR region LIKE :region) AND
+                                    (:victim_total_damage_received IS NULL OR victim_total_damage_received LIKE :victim_total_damage_received) AND
+                                    (:max_total_participants IS NULL OR total_participants <= :max_total_participants) AND
+                                    (:killer_ship_type IS NULL OR killer_ship_type LIKE :killer_ship_type) AND
+                                    (:killer_ship_category IS NULL OR killer_ship_category LIKE :killer_ship_category) AND
+                                    (:timestamp IS NULL OR timestamp LIKE :timestamp)
+                                    ORDER BY isk DESC
+                                    LIMIT 100
+                                  '''), params=params)
+
+    return render_template('search.html', kms=kms)
+    
+@app.before_first_request
+def database_tweak():
+    print("increasing cache size")
+    db.session.execute(text('PRAGMA cache_size=-200000'))
+
+
+app.register_blueprint(routes)
+
+if __name__ == "__main__":
+    app.run()
+
+if __name__ != "__main__":
+    gunicorn_logger = logging.getLogger('gunicorn.error')
+    app.logger.handlers = gunicorn_logger.handlers
+    app.logger.setLevel(gunicorn_logger.level)
